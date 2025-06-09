e9881f6be8fc90cef89c843c7a15dcb67607e7a7
Annie <annenark@gmail.com>
2025-06-09 15:27:16 -0500

    Initial Commit

---
+++ Dress A Pigeon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dress A Pigeon
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dress A Pigeon/Dress_A_PigeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dress_A_PigeonApp.swift
+//  Dress A Pigeon
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Dress_A_PigeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
