1c5c076b1474a61040aa3b8b1eb8968ad62261ab
Isabella <ibarron083@rsdmo.org>
2025-06-10 13:45:02 -0500

    I deleted everything because it was not working at all. sorry.

--- Dress A Pigeon/MainScreen.swift
+++ Dress A Pigeon/MainScreen.swift
@@ -8,36 +8,7 @@
 import SwiftUI
 struct MainScreen: View {
     var body: some View {
-        ZStack { // Layer views on top of each other
-            
-           
-            VStack {
-                NavigationStack {
-                    VStack(spacing: 40) {
-                        Image("MainScreenBackground")
-                            .resizable()
-                            .scaledToFill()
-                            .ignoresSafeArea()
-                        
-                        Text("Welcome!")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.purple)
-                            .shadow(radius: 10)
-                            .padding()
-
-                        NavigationLink(destination: HatView()) {
-                            Text("Start")
-                                .font(.title)
-                                .foregroundColor(Color.purple)
-                                .padding()
-                                .background(Color.white)
-                                .cornerRadius(10)
-                        }
-                    }
-                }
-            }
-        }
+        
     }
 }
 
